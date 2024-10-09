402a944e81e00e654e3d97f957f5a50e3f730b77
Zein Hajj-Ali <zeinhajjali@outlook.com>
2024-10-09 11:07:29 -0400

    Added remote-nvim plugin (have some .git file permission issues)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -344,4 +344,49 @@ return {
     "tpope/vim-fugitive",
     lazy = false,
   },
+  {
+    "amitds1997/remote-nvim.nvim",
+    event = "VeryLazy",
+    version = "0.3.9", -- Pin to GitHub releases
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- For standard functions
+      "MunifTanjim/nui.nvim", -- To build the plugin UI
+      "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+    },
+    config = function()
+      require("remote-nvim").setup {
+        ssh_config = {
+          scp_binary = "scp -O",
+        },
+        remote = {
+          copy_dirs = {
+            config = {
+              compression = {
+                enabled = true,
+                additional_opts = { "--exclude-vcs" },
+              },
+            },
+            data = {
+              compression = {
+                enabled = true,
+                additional_opts = { "--exclude-vcs" },
+              },
+            },
+            cache = {
+              compression = {
+                enabled = true,
+                additional_opts = { "--exclude-vcs" },
+              },
+            },
+            state = {
+              compression = {
+                enabled = true,
+                additional_opts = { "--exclude-vcs" },
+              },
+            },
+          },
+        },
+      }
+    end,
+  },
 }
